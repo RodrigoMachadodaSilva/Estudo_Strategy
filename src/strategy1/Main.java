29de2f9e68037d421185b3e69a48ad46f9430385
RodrigoMachadodaSilva <87843482+RodrigoMachadodaSilva@users.noreply.github.com>
2023-09-02 16:29:45 -0300

    Criação da classe Main para execução do código

--- src/strategy1/Main.java
+++ src/strategy1/Main.java
@@ -1,6 +1,19 @@
 package strategy1;
 
 public class Main {
+	
+	public static void main(String[] args) {
+	
+	int distancia = 100;
+	
+	int opcaoFrete = 0;
+	
+	TipoFrete tipoFrete = TipoFrete.values() [opcaoFrete];
+	
+	Frete frete = tipoFrete.obterFrete();
+	double preco = frete.calcularPreco(distancia);
+	System.out.print(preco);
 
+	}
 
 }
